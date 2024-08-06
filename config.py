b30844773511ea503f791becdaa007ad87a10715
Ropnbcxyujj <ggjkkiydf68@gmail.com>
2024-08-06 13:05:40 +0530

    Kkbvfghh

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7299596753:AAHlc_UY1yQqNVpmFLOGD34jA_Mi3qFckzY")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("venomXcrazy")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("2094826745")
+
+
+
+
+
+
+
+  
